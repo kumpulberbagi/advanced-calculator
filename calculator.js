1404a9ab10084d5066a36f8b395bce511fc94182
Windiana <windi@hacktiv8.com>
2016-09-23 15:06:45 +0700

    Template

---
+++ calculator.js
@@ -0,0 +1,13 @@
+"use strict";
+
+export class Calculator {
+  //write your code here
+}
+
+/** note : you can use several features from ecmascript, such as:
+* - Classes
+* - Default Parameters
+* - Destructured Assignment
+* - Template Literals
+* - Method Chaining
+*/
---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import {Calculator} from "./calculator.js";
+
+// execute function on calculator.js in here
