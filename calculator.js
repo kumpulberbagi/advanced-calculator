8ec3374eeb327c88506aedea00f65f84cc793e91
Ahyana Rizky Pratama & Alexander Hendrawan tjahja <pair+ahyanarizky.alexanderHT@devbootcamp.com>
2016-09-26 16:07:30 +0700

    tugas advance kalkulator

--- calculator.js
+++ calculator.js
@@ -1,7 +1,51 @@
 "use strict";
 
-export class Calculator {
+export class Calculator{
   //write your code here
+  constructor(value){
+    this._angka = value || 0;
+  }
+  //
+  set angka(value){
+    this._angka = value
+  }
+  get angka(){
+    return this._angka
+  }
+  add(parameter){
+     return parameter + this._angka;
+  }
+  sub(parameter){
+    return this._angka - parameter;
+  }
+
+  mul(parameter){
+    return this._angka * parameter;
+  }
+
+  div(parameter){
+    return this._angka / parameter;
+  }
+
+  rank(parameter){
+    return Math.pow(this._angka, parameter);
+  }
+
+  root(){
+    return Math.sqrt(this._angka);
+  }
+
+  circle(radius){
+    return Math.PI * radius
+  }
+
+
+
+// root(){
+//   console.log(Math.sqrt());
+// }
+
+
 }
 
 /** note : you can use several features from ecmascript, such as:
--- main.js
+++ main.js
@@ -2,4 +2,16 @@
 
 import {Calculator} from "./calculator.js";
 
+var calculator = new Calculator();
+calculator.angka = 9;
+console.log("penjumlahan : " + calculator.add(10));
+console.log("pengurangan : " +calculator.sub(4));
+console.log("pembagian : " +calculator.div(3));
+console.log("perkalian : " +calculator.mul(2));
+console.log("akar : " +calculator.root());
+console.log("pangkat : " +calculator.rank(2));
+console.log("luas lingkaran : " +calculator.circle(4));
+
+
+// calculator.sub();
 // execute function on calculator.js in here
