9c4bec51150b2042b1532bc06a7be6f8ae2fd0d8
Toni & Elkana Lumbantobing <pair+chrisantoni.elkana-ltobing@devbootcamp.com>
2016-09-26 16:01:56 +0700

    toni elkana

--- calculator.js
+++ calculator.js
@@ -1,7 +1,52 @@
 "use strict";
 
+const PI = 3.14
+
 export class Calculator {
-  //write your code here
+constructor()
+{
+  this.i = 0
+}
+
+  penjumlahan(a)
+  {
+    this.i += a
+  }
+
+  pengurangan(a)
+  {
+    this.i -= a
+  }
+
+  perkalian(a)
+  {
+    this.i *= a
+  }
+
+  pembagian(a)
+  {
+    this.i /= a
+  }
+
+  pangkat(a)
+  {
+    this.i = Math.pow(i,a)
+  }
+
+  akarPangkat(a)
+  {
+    this.i = Math.sqrt(a)
+  }
+
+  luasLingkaran(a)
+  {
+    this.i =  PI*(Math.pow(a,2))
+  }
+
+  print()
+  {
+    console.log(this.i);
+  }
 }
 
 /** note : you can use several features from ecmascript, such as:
--- main.js
+++ main.js
@@ -3,3 +3,10 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+
+let myCalculator = new Calculator()
+
+myCalculator.penjumlahan(5)
+myCalculator.pengurangan(2)
+myCalculator.perkalian(2)
+myCalculator.print()
