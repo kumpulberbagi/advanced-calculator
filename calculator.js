52b0500d690869b6d11203f22af0d9bda9e416fb
Aji Lantang <ajilantang@gmail.com>
2016-09-26 14:40:40 +0700

    aji dan tama advance calculator

--- calculator.js
+++ calculator.js
@@ -1,9 +1,34 @@
 "use strict";
 
 export class Calculator {
+  constructor(x, y){
+    this._x = x;
+    this._y = y;
+  }
   //write your code here
+  addition() {
+      return this._x+this._y;
+  }
+  substraction(){
+    return this._x-this._y;
+  }
+  multiplication() {
+    return this._x*this._y;
+  }
+  division() {
+    return this._x/this._y;
+  }
+  power() {
+    return Math.pow(this._x, this._y);
+  }
+  squareRoot() {
+    return Math.sqrt(this._x, this._y);
+  }
+  area(){
+    return this._x*this._x*Math.PI;
+  }
 }
-
+//
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
--- main.js
+++ main.js
@@ -3,3 +3,12 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+let Advanced = new Calculator(4,2);
+
+console.log(Advanced.addition());
+console.log(Advanced.substraction());
+console.log(Advanced.multiplication());
+console.log(Advanced.division());
+console.log(Advanced.squareRoot());
+console.log(Advanced.area());
+//..
