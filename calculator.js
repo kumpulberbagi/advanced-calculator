3dbef6d9e4a5c74f3eed654b093c0e8f96873ca8
name": null <email": null>
2016-09-26 20:31:39 +0700

    calculator

--- calculator.js
+++ calculator.js
@@ -40,16 +40,16 @@ export class Calculator {
 }
 
 
-// var calculator = new Calculator();
-// calculator.a = 8;
+var calculator = new Calculator();
+calculator.a = 8;
 
 // console.log(calculator.penjumlahan(5).penjumlahan(3));
-// console.log(calculator.pengurangan(2));
-// console.log(calculator.perkalian(2));
-// console.log(calculator.pembagian(2));
-// console.log(calculator.pangkat(2));
-// console.log(calculator.akar(2));
-// console.log(calculator.luasLingkaran(5));
+console.log(calculator.pengurangan(2));
+console.log(calculator.perkalian(2));
+console.log(calculator.pembagian(2));
+console.log(calculator.pangkat(2));
+console.log(calculator.akar(2));
+console.log(calculator.luasLingkaran(5));
 
 /** note : you can use several features from ecmascript, such as:
  * - Classes
