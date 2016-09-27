c99e27473f1031480710d4a7107729c8989377b7
Alessandro C & Juang Wiantoro <pair+alessandrocls.broerjuang@hacktiv8.com>
2016-09-27 10:48:07 +0700

    Submit Method Chaining for Advance Calculator

--- calculator.js
+++ calculator.js
@@ -2,7 +2,49 @@
 
 export class Calculator {
   //write your code here
+  //write your code here
+    constructor() {
+      this.init = 0;
+    }
+
+    set x(value) {
+      this._x = value;
+    }
+
+    get x() {
+      return this.x;
+    }
+
+    add(x) {
+      this.init += x;
+      return this;
+    }
+
+    substract(x) {
+      this.init -= x;
+      return this;
+    }
+
+    multiply(x) {
+      this.init *= x;
+      return this;
+    }
+
+    square(x) {
+      this.init = Math.pow(this.init, x);
+      return this;
+    }
+
+    sqrt() {
+      this.init = Math.sqrt(this.init);
+      return this;
+    }
+
+    print() {
+      console.log(this.init);
+    }
 }
+
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
--- main.js
+++ main.js
@@ -3,3 +3,5 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+let a = new Calculator();
+a.add(3).add(2).square(2).print();
