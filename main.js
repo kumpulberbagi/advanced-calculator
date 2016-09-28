8b2e7461dc592819648f7bf0fd3f2b44194a61a7
Toni <tony_chen93@yahoo.com>
2016-09-28 08:59:50 +0700

    update main.js
    added method chain

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import {Calculator} from "./calculator.js";
 // execute function on calculator.js in here
 
 let myCalculator = new Calculator()
-
+myCalculator.num = 2
 myCalculator.penjumlahan(5).pengurangan(2).perkalian(2).print()
 // myCalculator.penjumlahan(5)
 // myCalculator.pengurangan(2)
