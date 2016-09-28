cc93820858efb7775e646ffffed8dcda3e6c1a7c
Toni <tony_chen93@yahoo.com>
2016-09-28 08:30:43 +0700

    Update calculator.js
    added chaining method

--- calculator.js
+++ calculator.js
@@ -11,36 +11,43 @@ constructor()
   penjumlahan(a)
   {
     this.i += a
+    return this
   }
 
   pengurangan(a)
   {
     this.i -= a
+    return this
   }
 
   perkalian(a)
   {
     this.i *= a
+    return this
   }
 
   pembagian(a)
   {
     this.i /= a
+    return this
   }
 
   pangkat(a)
   {
     this.i = Math.pow(this.i,a)
+    return this
   }
 
   akarPangkat(a)
   {
     this.i = Math.sqrt(a)
+    return this
   }
 
   luasLingkaran(a)
   {
     this.i =  PI*(Math.pow(a,2))
+    return this
   }
 
   print()
