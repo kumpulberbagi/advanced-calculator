db916ec997f9df3a796cf3ab7c0b9dc9c7001805
Ahyana Rizky Pratama & YONI SETIAWAN <pair+ahyanarizky.yonisetiawan@devbootcamp.com>
2016-09-28 08:47:06 +0700

    calculator finally done

--- calculator.js
+++ calculator.js
@@ -3,36 +3,56 @@
 export class Calculator {
   //write your code here
   constructor(num1){
-    this.num1 = num1;
+    this._num1 = num1;
 
   }
+
+  set num1(value){
+    this._num1 = value;
+  }
+
+  get num1(){
+    return this._num1;
+  }
+
   perkalian(num2){
-    return this.num1 * num2;
+     this._num1 = this._num1 * num2;
+     return this
   }
 
   pengurangan(num2){
-    return this.num1 - num2
-
+    this._num1 = this._num1 - num2;
+    return this
   }
 
   penjumlahan(num2){
-    return this.num1 + num2
+    this._num1 = this._num1 + num2;
+    return this
   }
 
   pembagian(num2){
-    return this.num1 / num2
+    this._num1= this._num1 / num2;
+    return this
   }
 
   pangkat(num2){
-    return Math.pow(this.num1,num2)
+    this._num1 = Math.pow(this._num1,num2);
+    return this
   }
 
   akar(){
-    return Math.sqrt(this.num1)
+    this._num1 = Math.sqrt(this._num1)
+    return this
   }
 
   pi(){
-    return (Math.pow(this.num1,2)) * Math.PI
+    this._num1 = Math.pow(this._num1,2)* Math.PI
+    return this
+  }
+
+  hasil(){
+    console.log(this._num1)
+
   }
 
 }
--- main.js
+++ main.js
@@ -3,12 +3,11 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
-let hitung = new Calculator(6);
+let hitung = new Calculator();
 
-console.log(hitung.perkalian(5));
-console.log(hitung.pengurangan(5));
-console.log(hitung.penjumlahan(5));
-console.log(hitung.pembagian(5));
-console.log(hitung.pangkat(5));
-console.log(hitung.akar());
-console.log(hitung.pi());
+hitung.num1 = 2
+
+//console.log(hitung.num1);
+
+hitung.perkalian(5).pengurangan(5).pembagian(5).pengurangan(5).penjumlahan(5)
+      .pembagian(2).pangkat(5).akar().pi().hasil();
