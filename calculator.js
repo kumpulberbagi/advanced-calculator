3886ac48f733500bf1fb62fbce1af9bf583d9f7e
broerjuang <juangwiantoro@gmail.com>
2016-10-02 17:11:35 +0700

    Submit Advance Calculator

--- calculator.js
+++ calculator.js
@@ -2,6 +2,37 @@
 
 export class Calculator {
   //write your code here
+    constructor(value) {
+      this.init = value;
+    }
+
+    add(x) {
+      this.init += x;
+      return this;
+    }
+
+    substract(x) {
+      this.init -= x;
+      return this;
+    }
+
+    multiply(x) {
+      this.init *= x;
+      return this;
+    }
+
+    square(x) {
+      this.init = Math.pow(this.init, x);
+      return this;
+    }
+
+    sqrt() {
+      this.init = Math.sqrt(this.init);
+      return this;
+    }
+    print() {
+      console.log(this.init);
+    }
 }
 
 /** note : you can use several features from ecmascript, such as:
@@ -11,3 +42,8 @@ export class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+
+
+var c = new Calculator();
+
+c.add(2).add(2).sqrt().print();
--- main.js
+++ main.js
@@ -3,3 +3,6 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+var c = new Calculator();
+
+c.add(2).add(2).sqrt().print();
